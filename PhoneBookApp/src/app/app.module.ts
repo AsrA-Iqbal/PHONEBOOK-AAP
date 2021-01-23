f66735a7f499186497e2387e06c318b81838c434
ASRA IQBAL <asraiqbalofficial@gmail.com>
2021-01-24 02:47:22 +0500

    SetUP
    Project Setup

---
+++ PhoneBookApp/src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
+import { ContactRecordComponent } from './contact-record/contact-record.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTableModule} from '@angular/material/table';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatIconModule} from '@angular/material/icon';
+import { MatDialogModule} from '@angular/material/dialog';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    AddContactComponent,
+    ContactRecordComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatDialogModule,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ PhoneBookApp/src/app/contact-record/contact-record.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-record',
+  templateUrl: './contact-record.component.html',
+  styleUrls: ['./contact-record.component.scss']
+})
+export class ContactRecordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
